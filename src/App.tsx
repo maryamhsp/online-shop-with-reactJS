f5d0ed105390fcaea9e2dc28ce6e58917b650d88
maryamhsp <m.hosseinpoor01@gmail.com>
2026-01-13 13:50:07 +0200

    feat: setup Tailwind CSS and add initial navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Route,Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Store from "./pages/store/Store"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
   
   return (
-   <>
-   <h1>Mari Shop</h1>
-   </>
+    <>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/store" element={<Store/>} />
+    </Routes>
+    </>
   )
 }
 
